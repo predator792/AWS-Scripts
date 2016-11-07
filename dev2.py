aa39143c9e2af09001636d40dd8da6f737c95749
predator792 <vzv0012@auburn.edu>
2016-11-06 20:37:00 -0600

    Update dev2.py to pass the keyword in boto3

--- dev2.py
+++ dev2.py
@@ -2,4 +2,4 @@
 from boto3.session import Session
 session= Session(aws_access_key_id='USEMYID', aws_secret_access_key='USEYOURACCESSKEYS')
 s3 =session.resource('s3')
-s3.create_bucket=('myfirstlabucket7')
+s3.create_bucket(Bucket='myfirstlabucket7')
